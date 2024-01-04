a78908c379aa8afcc14880d5d6cdcbc5e2fb39de
oscar hernandez <ohdz202@gmail.com>
2024-01-03 18:06:11 -0600

    Se modifica la URL del json server

--- services/client-services.js
+++ services/client-services.js
@@ -1,43 +1,61 @@
+const listaProductos = () =>
+  fetch("https://api-json-server-five.vercel.app/productos").then((respuesta) =>
+    respuesta.json()
+  );
 
-const listaProductos = () => fetch("http://localhost:3000/productos").then( (respuesta) => respuesta.json());
-
-const nuevoProducto = (nombre, precio, url__img, descripcion, categoria) =>{
-    return fetch("http://localhost:3000/productos", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({nombre, precio, url__img, descripcion, categoria, id: uuid.v4()})
-    });
+const nuevoProducto = (nombre, precio, url__img, descripcion, categoria) => {
+  return fetch("https://api-json-server-five.vercel.app/productos", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      nombre,
+      precio,
+      url__img,
+      descripcion,
+      categoria,
+      id: uuid.v4(),
+    }),
+  });
 };
 
 const eliminarProd = (id) => {
-    console.log("eliminar a: ", id);
-    return fetch(`http://localhost:3000/productos/${id} `, {
-        method: "DELETE"
-    });
+  console.log("eliminar a: ", id);
+  return fetch(`https://api-json-server-five.vercel.app/productos/${id} `, {
+    method: "DELETE",
+  });
 };
 
 const verProducto = (id) => {
-    return fetch(`http://localhost:3000/productos/${id}`).then( (respuesta) => respuesta.json());
-}
+  return fetch(`https://api-json-server-five.vercel.app/productos/${id}`).then(
+    (respuesta) => respuesta.json()
+  );
+};
 
-const actulizarProd = (nombre, precio,url__img,descripcion, categoria,id) => {
-    return fetch(`http://localhost:3000/productos/${id}`,{
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({nombre, precio,url__img, descripcion,categoria})
-    })
+const actulizarProd = (
+  nombre,
+  precio,
+  url__img,
+  descripcion,
+  categoria,
+  id
+) => {
+  return fetch(`https://api-json-server-five.vercel.app/productos/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ nombre, precio, url__img, descripcion, categoria }),
+  })
     .then((respuesta) => respuesta)
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 };
 
 export const clientServices = {
-    listaProductos,
-    nuevoProducto,
-    eliminarProd,
-    verProducto,
-    actulizarProd
+  listaProductos,
+  nuevoProducto,
+  eliminarProd,
+  verProducto,
+  actulizarProd,
 };
